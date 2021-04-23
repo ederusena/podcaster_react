9b96ca796ae24ef851378cedf05877b9ff16068b
ederusena <eder.sena@live.com>
2021-04-23 14:13:35 -0300

    chore: botão com props children

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Button from './Button';
+
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+      <Button>Click 1</Button>
+      <Button>Click 2</Button>
+    </>
   );
 }
 
