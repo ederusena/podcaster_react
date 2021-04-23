6e642b15c03c750b82322eacd0b816e9d75ccb7a
ederusena <eder.sena@live.com>
2021-04-23 14:24:19 -0300

    chore: useState

--- src/Button.js
+++ src/Button.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import { useState } from 'react'
 
 export default function Button(props) {
+  const [counter, setCounter] = useState(1);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
   return (
     <>
-      <button>{props.title}</button>
+      <span>{counter}</span>
+      <button onClick={increment}>{props.title}</button>
+      <br></br>
     </>
   )
 }
